3994a3b69faf2afb7936a7cd41cfcef7466caedc
Martynas Petuska <martynas.petuska@gamma.co.uk>
2018-03-31 14:10:00 +0100

    ADMIN

---
+++ docs/ISSUE_TEMPLATE.md
@@ -0,0 +1,9 @@
+## Description
+
+
+## Technical Description
+
+
+************************
+#### Prerequisites: 
+#### Pull request: TBC
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,1 @@
-CREATE TABLE IF NOT EXISTS student
-(
-  id              INTEGER      NOT NULL,
-  name            VARCHAR(255) NOT NULL,
-  passport_number VARCHAR(255),
-  PRIMARY KEY (id)
-);
+RUNSCRIPT FROM 'classpath:sql/tables/student.sql';
